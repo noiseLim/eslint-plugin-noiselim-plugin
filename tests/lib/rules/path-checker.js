5d07610aaa3abd6a80b782ed52be93c8fd42b147
noiseLim <noiselim@gmail.com>
2023-04-25 17:56:36 +0300

    added support for aliases

--- tests/lib/rules/path-checker.js
+++ tests/lib/rules/path-checker.js
@@ -2,30 +2,53 @@
  * @fileoverview feature sliced relative path checker
  * @author Aleksey
  */
-"use strict";
+'use strict';
 
 //------------------------------------------------------------------------------
 // Requirements
 //------------------------------------------------------------------------------
 
-const rule = require("../../../lib/rules/path-checker"),
-  RuleTester = require("eslint").RuleTester;
-
+const rule = require('../../../lib/rules/path-checker'),
+  RuleTester = require('eslint').RuleTester;
 
 //------------------------------------------------------------------------------
 // Tests
 //------------------------------------------------------------------------------
 
-const ruleTester = new RuleTester();
-ruleTester.run("path-checker", rule, {
+const ruleTester = new RuleTester({
+  parserOptions: { ecmaVersion: 6, sourceType: 'module' },
+});
+ruleTester.run('path-checker', rule, {
   valid: [
-    // give me some code that won't trigger a warning
+    {
+      filename: 'C:\\Users\\name\\Desktop\\project\\src\\entities\\Article',
+      code: "import { addCommentFormActions, addCommentFormReducer } from '../../model/slice/addCommentFormSlice';",
+      errors: [],
+    },
   ],
-
   invalid: [
     {
-      code: "qwerty",
-      errors: [{ message: "Fill me in.", type: "Me too" }],
+      filename: 'C:\\Users\\name\\Desktop\\project\\src\\entities\\Article',
+      code: "import { addCommentFormActions, addCommentFormReducer } from 'entities/Article/model/slice/addCommentFormSlice';",
+      errors: [
+        {
+          message: 'Within a once slice, all paths must be relative',
+        },
+      ],
+      options: [
+        {
+          alias: '@',
+        },
+      ],
+    },
+    {
+      filename: 'C:\\Users\\name\\Desktop\\project\\src\\entities\\Article',
+      code: "import { addCommentFormActions, addCommentFormReducer } from 'entities/Article/model/slice/addCommentFormSlice';",
+      errors: [
+        {
+          message: 'Within a once slice, all paths must be relative',
+        },
+      ],
     },
   ],
 });
