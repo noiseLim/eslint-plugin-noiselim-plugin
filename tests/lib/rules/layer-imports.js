c6527115c22faadcf7363422c685c448af6dd7b1
noiseLim <noiselim@gmail.com>
2023-04-26 21:47:17 +0300

    added loyer imports rule

---
+++ tests/lib/rules/layer-imports.js
@@ -0,0 +1,98 @@
+const rule = require('../../../lib/rules/layer-imports'),
+  RuleTester = require('eslint').RuleTester;
+
+const aliasOptions = [
+  {
+    alias: '@',
+  },
+];
+const ruleTester = new RuleTester({
+  parserOptions: { ecmaVersion: 6, sourceType: 'module' },
+});
+ruleTester.run('layer-imports', rule, {
+  valid: [
+    {
+      filename: 'C:\\Users\\user\\Desktop\\project\\src\\features\\Article',
+      code: "import { addCommentFormActions, addCommentFormReducer } from '@/shared/Button.tsx'",
+      errors: [],
+      options: aliasOptions,
+    },
+    {
+      filename: 'C:\\Users\\user\\Desktop\\project\\src\\features\\Article',
+      code: "import { addCommentFormActions, addCommentFormReducer } from '@/entities/Article'",
+      errors: [],
+      options: aliasOptions,
+    },
+    {
+      filename: 'C:\\Users\\user\\Desktop\\project\\src\\app\\providers',
+      code: "import { addCommentFormActions, addCommentFormReducer } from '@/widgets/Article'",
+      errors: [],
+      options: aliasOptions,
+    },
+    {
+      filename: 'C:\\Users\\user\\Desktop\\project\\src\\widgets\\pages',
+      code: "import { useLocation } from 'react-router-dom'",
+      errors: [],
+      options: aliasOptions,
+    },
+    {
+      filename: 'C:\\Users\\user\\Desktop\\project\\src\\app\\providers',
+      code: "import { addCommentFormActions, addCommentFormReducer } from 'redux'",
+      errors: [],
+      options: aliasOptions,
+    },
+    {
+      filename: 'C:\\Users\\user\\Desktop\\project\\src\\index.tsx',
+      code: "import { StoreProvider } from '@/app/providers/StoreProvider';",
+      errors: [],
+      options: aliasOptions,
+    },
+    {
+      filename: 'C:\\Users\\user\\Desktop\\project\\src\\entities\\Article.tsx',
+      code: "import { StateSchema } from '@/app/providers/StoreProvider'",
+      errors: [],
+      options: [
+        {
+          alias: '@',
+          ignoreImportPatterns: ['**/StoreProvider'],
+        },
+      ],
+    },
+  ],
+
+  invalid: [
+    {
+      filename: 'C:\\Users\\user\\Desktop\\project\\src\\entities\\providers',
+      code: "import { addCommentFormActions, addCommentFormReducer } from '@/features/Article'",
+      errors: [
+        {
+          message:
+            'A layer can import only the underlying layers (shared, entities, features, widgets, pages, app)',
+        },
+      ],
+      options: aliasOptions,
+    },
+    {
+      filename: 'C:\\Users\\user\\Desktop\\project\\src\\features\\providers',
+      code: "import { addCommentFormActions, addCommentFormReducer } from '@/widgets/Article'",
+      errors: [
+        {
+          message:
+            'A layer can import only the underlying layers (shared, entities, features, widgets, pages, app)',
+        },
+      ],
+      options: aliasOptions,
+    },
+    {
+      filename: 'C:\\Users\\user\\Desktop\\project\\src\\entities\\providers',
+      code: "import { addCommentFormActions, addCommentFormReducer } from '@/widgets/Article'",
+      errors: [
+        {
+          message:
+            'A layer can import only the underlying layers (shared, entities, features, widgets, pages, app)',
+        },
+      ],
+      options: aliasOptions,
+    },
+  ],
+});
